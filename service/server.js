14ed526e1f159eced30078ba167f30a91ff270b9
Matthew Moya <matthewmoya@Matthews-MacBook-Pro.local>
2019-06-20 20:37:10 -0700

    attach handlers

--- service/server.js
+++ service/server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const recordGame = require('./handlers/game/create');
+const listGames = require('./handlers/game/read');
+
+const createUser = require('./handlers/user/create');
+const listUsers = require('./handlers/user/create');
+
+const getLeaderboard = require('./handlers/leaderboard/read');
+const refreshSystem = require('./handlers/system/update');
+
 const port = process.env.PORT || 3000; // 4000 for local.
 const app = express();
-const actions = new Actions();
 
 app.use(bodyParser.json({limit: '10mb', extended: true}));
 app.use(cors({
@@ -19,13 +27,17 @@ const asyncHandler = fn => (req, res, next) =>
     
 app.get('/ping', asyncHandler(async (req, res, next) => res.status(200).send(`pong`)));
 
-app.get('/player', asyncHandler(async (req, res, next) => res.send(await actions.getPlayers(req))));
-app.get('/player/:player', asyncHandler(async (req, res, next) => res.send(await actions.getPlayer(req))));
-app.post('/player', asyncHandler(async (req, res, next) => res.send(await actions.addPlayer(req))));
-app.get('/leaderboard', asyncHandler(async (req, res, next) => res.send(await actions.getLeaderboard(req))));
-app.get('/game', asyncHandler(async (req, res, next) => res.send(await actions.getGameLog(req))));
-app.post('/game', asyncHandler(async (req, res, next) => res.send(await actions.postResult(req))));
-app.put('/refresh', asyncHandler(async (req, res, next) => res.send(await actions.refreshService(req))));
+// Combine these two into 1
+app.get('/player', asyncHandler(async (req, res, next) => res.send(await listUsers(req))));
+app.get('/player/:player', asyncHandler(async (req, res, next) => res.send(await listUsers(req))));
+app.post('/player', asyncHandler(async (req, res, next) => res.send(await createUser(req))));
+
+app.get('/leaderboard', asyncHandler(async (req, res, next) => res.send(await getLeaderboard(req))));
+
+app.get('/game', asyncHandler(async (req, res, next) => res.send(await listGames(req))));
+app.post('/game', asyncHandler(async (req, res, next) => res.send(await recordGame(req))));
+
+app.put('/refresh', asyncHandler(async (req, res, next) => res.send(await refreshSystem(req))));
 
 app.use((err, req, res, next) => {
   console.error(err.stack)
